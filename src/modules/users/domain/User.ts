e88bae82a2d6855b0201bbb0456256c50cd18c00
shinya fukimbara <progriro@gmail.com>
2022-11-13 16:34:27 +0900

    chore: remove public values

--- src/modules/users/domain/User.ts
+++ src/modules/users/domain/User.ts
@@ -2,6 +2,7 @@ import { Entity } from "../../../shared/domain/Entity";
 import { UniqueID } from "../../../shared/domain/UniqueID";
 
 type UserProps = {
+  id: UniqueID;
   name: string;
   email: string;
   password: string;
@@ -11,33 +12,26 @@ type UserProps = {
 };
 
 export class User extends Entity<UserProps> {
-  public readonly id: UniqueID;
-  public readonly name: string;
-  public readonly email: string;
-  public readonly password: string;
-  public readonly role: string;
-  public readonly createdAt: Date;
-  public readonly updatedAt: Date;
-
   constructor(props: UserProps, id?: string) {
     super(props, id);
   }
 
   public changeName(newName: string) {
-    return new User({ ...this.props, name: newName }, this.id.value);
+    return new User({ ...this.props, name: newName }, this.props.id.value);
   }
 
   public changeEmail(newEmail: string) {
-    return new User({ ...this.props, email: newEmail }, this.id.value);
+    return new User({ ...this.props, email: newEmail }, this.props.id.value);
   }
 
   public changePassword(newPassword: string) {
-    return new User({ ...this.props, password: newPassword }, this.id.value);
+    return new User(
+      { ...this.props, password: newPassword },
+      this.props.id.value
+    );
   }
 
   public changeRole(newRole: string) {
-    return new User({ ...this.props, role: newRole }, this.id.value);
+    return new User({ ...this.props, role: newRole }, this.props.id.value);
   }
-
-  // いっぱいメソッドが来ると良いね！
 }
