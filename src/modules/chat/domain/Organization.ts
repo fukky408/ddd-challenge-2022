8de6acaf0cc0d83c654f27942e0b7ee70886f436
unvalley <kirohi.code@gmail.com>
2022-11-17 00:04:05 +0900

    refactor: remove id from props

--- src/modules/chat/domain/Organization.ts
+++ src/modules/chat/domain/Organization.ts
@@ -1,9 +1,6 @@
-// Otional: 自分たちで考えたものなのであんまり重要ではない
 import { Entity } from "../../../shared/domain/Entity";
-import { UniqueID } from "../../../shared/domain/UniqueID";
 
 type OrganizationProps = {
-  id: UniqueID;
   name: string;
   createdAt?: Date;
 };
