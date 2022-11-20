bc00cbaaa0deff1b0928923f4e83aa1fd853eb86
unvalley <kirohi.code@gmail.com>
2022-11-20 21:33:47 +0900

    feat: ListSchdeuldMessages

--- src/modules/chat/useCases/DeleteScheduledMessage.ts
+++ src/modules/chat/useCases/DeleteScheduledMessage.ts
@@ -1,27 +1,33 @@
 import { UseCase } from "../../../shared/core/UseCase";
-import { IScheduledChatMessageRepo } from "../repositories/IScheduledChatMessageRepo"
+import { IScheduledChatMessageRepo } from "../repositories/IScheduledChatMessageRepo";
 
 type Request = {
   scheduledMessageId: string;
   userId: string;
-}
+};
 
 type Response = boolean;
 
-export class DeleteScheduledMessage implements UseCase<Request, Promise<Response>> {
-  constructor(
-    private scheduledChatMessageRepo: IScheduledChatMessageRepo
-  ) { }
+export class DeleteScheduledMessage
+  implements UseCase<Request, Promise<Response>>
+{
+  constructor(private scheduledChatMessageRepo: IScheduledChatMessageRepo) {}
 
   public async execute(request: Request): Promise<Response> {
-    const msg = await this.scheduledChatMessageRepo.findById(request.scheduledMessageId)
+    const msg = await this.scheduledChatMessageRepo.findById(
+      request.scheduledMessageId
+    );
     if (!msg) {
-      throw new Error(`scheduledMessageId=${request.scheduledMessageId}) not found.`);
+      throw new Error(
+        `scheduledMessageId=${request.scheduledMessageId}) not found.`
+      );
     }
     if (!(msg.senderId === request.userId)) {
-      throw `request userId=${request.userId} is not match to scheduled message userId=${msg.senderId}`
+      throw new Error(
+        `userId=${request.userId} does not match to scheduled message userId=${msg.senderId}`
+      );
     }
-    const res = await this.scheduledChatMessageRepo.delete(msg.id.value)
-    return res
+    const res = await this.scheduledChatMessageRepo.delete(msg.id.value);
+    return res;
   }
 }
