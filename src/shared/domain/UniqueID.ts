13f7503447bfab613b9fdecfee3edf62aa74c497
shinya fukimbara <progriro@gmail.com>
2022-11-20 23:32:13 +0900

    refactor: UniqueID

--- src/shared/domain/UniqueID.ts
+++ src/shared/domain/UniqueID.ts
@@ -1,17 +1,14 @@
 import { uuid } from "uuidv4";
 import * as zod from "zod";
-import { ValueObject } from "./ValueObject";
 
 export const idSchema = zod.string().uuid();
 
 export type IdValue = zod.infer<typeof idSchema>;
 
-export class UniqueID extends ValueObject<IdValue | undefined> {
+export class UniqueID {
   public value: IdValue;
 
   constructor(value: IdValue | undefined) {
-    super(value);
-
     if (value) {
       idSchema.parse(value);
       Object.freeze(this);
